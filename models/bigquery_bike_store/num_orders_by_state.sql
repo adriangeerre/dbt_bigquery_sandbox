e8a6f7b88ed6d9e0c324b6489c834daa56e3cc56
adriangeerre <adriangomezrepolles@gmail.com>
2025-08-27 11:01:30 +0200

    update comment

--- models/bigquery_bike_store/num_orders_by_state.sql
+++ models/bigquery_bike_store/num_orders_by_state.sql
@@ -1,8 +1,8 @@
--- SELECT zip_code, COUNT(order_id) as num_orders
+-- SELECT state, city, COUNT(order_id) as num_orders
 -- FROM `bike_store.customers` as customers
 -- JOIN `bike_store.orders` as orders
 -- ON customers.customer_id = orders.customer_id
--- GROUP BY zip_code
+-- GROUP BY state, city
 -- ORDER BY num_orders DESC;
 
 with order_by_city as (
