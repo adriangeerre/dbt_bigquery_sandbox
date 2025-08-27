5ce3b1b26877220e5998eb53b4cba30986569a38
adriangeerre <adriangomezrepolles@gmail.com>
2025-08-27 09:43:57 +0200

    orders by city

--- models/num_orders_by_state.sql
+++ models/num_orders_by_state.sql
@@ -5,17 +5,17 @@
 -- GROUP BY zip_code
 -- ORDER BY num_orders DESC;
 
-with order_by_zipcode as (
-    SELECT zip_code, COUNT(order_id) as num_orders
+with order_by_city as (
+    SELECT state, city, COUNT(order_id) as num_orders
     FROM {{ source('bike_store', 'customers') }} as customers
     JOIN {{ source('bike_store', 'orders') }} as orders
     ON customers.customer_id = orders.customer_id
-    GROUP BY zip_code
+    GROUP BY state, city
     ORDER BY num_orders DESC
 ),
 
 final as (
-    select * from order_by_zipcode
+    select * from order_by_city
 ) 
 
 select * from final
